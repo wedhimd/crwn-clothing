9d9396da3947dda1c168d6bce7957b6dbfcc80b0
wedhimd <wedhimd@gmail.com>
2021-11-11 10:05:53 +0200

    update menu-item component to have navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homePage/homePage';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
-
 export default App;
